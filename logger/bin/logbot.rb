bf69c6a61116ccd28fb642f2844beabdb1fbeb90
k-kurumi <optpia.kurumi@gmail.com>
2015-06-18 15:55:50 +0900

    use ENV

--- logger/bin/logbot.rb
+++ logger/bin/logbot.rb
@@ -4,5 +4,15 @@ require 'pry'
 
 SLACK_TOKEN = ENV['SLACK_TOKEN']
 
-logger = SlackLogger::Bot.new(slack_token: SLACK_TOKEN)
+FLUENTD_HOST = ENV['FLUENTD_HOST']
+FLUENTD_PORT = ENV['FLUENTD_PORT']
+FLUENTD_TAG  = ENV['FLUENTD_TAG']
+
+logger = SlackLogger::Bot.new(
+  slack_token:  SLACK_TOKEN,
+  fluentd_host: FLUENTD_HOST,
+  fluentd_port: FLUENTD_PORT,
+  fluentd_tag:  FLUENTD_TAG
+)
+
 logger.exec
